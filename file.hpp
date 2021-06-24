35dc08888804b3a127baea66f6ff13b6875b3b12
FrowsyFrog <frowsyfrog@gmail.com>
2021-06-24 10:33:47 -0500

    Finished folder class

--- file.hpp
+++ file.hpp
@@ -1,5 +1,7 @@
 #include <list>
 #include <string>
+#include <filesystem>
+namespace fs = std::filesystem;
 using namespace std;
 
 enum class FileType {
@@ -12,7 +14,9 @@ protected:
   string path;
   string name;
   list<File*> versions;
-  File(string name, string path): name(name), path(path){}
+  File(string name, string path): name(name), path(path){
+    fs::create_directories(path);
+  }
 public:
   bool compareName(string name) {
     return this->name == name;
--- folder.hpp
+++ folder.hpp
@@ -6,13 +6,21 @@ class Folder : public File {
   list<File*> files;
 public:
   Folder(string path, string name) : File(path, name) {}
-  File* find(string path) {
-
+  File* find(string searchPath) {
+    string totalPath = getTotalPath(path, searchPath);
+    if (fileExists(totalPath)) {
+      string fileName = fs::path(totalPath).filename().generic_u8string();
+      // FALTA REEMPLAZAR LA BUSQUEDA FOR POR UNA DE ARBOLES
+      for (File* file : files) {
+        if (file->compareName(fileName)) return file;
+      }
+    }
     return nullptr;
   }
 
   bool contains(string name) {
     if (compareName(name)) return true;
+    // FALTA REEMPLAZAR LA BUSQUEDA FOR POR UNA DE ARBOLES
     for (File* file : files) {
       if (file->getFileType() == FileType::Document && file->compareName(name)) return true;
       else if (file->getFileType() == FileType::Folder) {
@@ -25,6 +33,18 @@ public:
 
   FileType getFileType() override {
     return FileType::Folder;
+  }
+private:
+  bool fileExists(const fs::path& p, fs::file_status s = fs::file_status {}) {
+    if (fs::status_known(s) ? fs::exists(s) : fs::exists(p)) return true;
+    return false;
+  }
+
+  string getTotalPath(string initialPath, string searchPath) {
+    string totalPath = initialPath;
+    totalPath += "/";
+    totalPath += searchPath;
+    return totalPath;
   }
 };
 
