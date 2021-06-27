b2d47eafe3f783fd662ed8b1fc4de0617a7b7dcd
FrowsyFrog <frowsyfrog@gmail.com>
2021-06-27 11:34:27 -0500

    Added Pragma Once and Commit methods

--- commit.hpp
+++ commit.hpp
@@ -1,15 +1,43 @@
-#include <string>
-#include <list>
-using namespace std;
+#pragma once
+#include "file.hpp"
+#include <chrono>
 
 class Commit {
+  //Informacion del usuario
   string sha1;
-  int timestap;
+  int timestamp;
   list<File*> changes;
 public:
-  Commit() {}
-  string getDiff() {
+  Commit(list<File*> stagingFiles) {
+    changes = stagingFiles;
+    timestamp = generateTimestamp(); //UNIX formato 
+    sha1 = hash();
+  }
 
+  string getDiff() {
+    return "";
+  }
+
+  string getSha1() {
+    return "";
+  }
+
+  int getTimestamp() {
+    return timestamp;
+  }
+
+  list<File*> getChanges() {
+    return changes;
+  }
+
+private:
+  int generateTimestamp() {
+    const auto timeNow = chrono::system_clock::now();
+    return chrono::duration_cast<chrono::seconds>(timeNow.time_since_epoch()).count();
+  }
+
+  string hash() {
+    return "";
   }
 
 };
--- document.hpp
+++ document.hpp
@@ -1,3 +1,4 @@
+#pragma once
 #include <file.hpp>
 
 class Document : public File {
