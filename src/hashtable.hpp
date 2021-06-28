4b496b597bc6d7eda94e6d80dcf155252b45bedd
Angel Meneses <angelmenesest123@gmail.com>
2021-06-28 18:58:11 -0500

    Revert "Fixed format"
    This reverts commit 0df15907a563e14ca5dcdd30e423d875e0cccb7a.

--- src/hashtable.hpp
+++ src/hashtable.hpp
@@ -5,127 +5,94 @@
 #include <stdexcept>
 #include <functional>
 
-using std::function;
-using std::list;
-using std::ostream;
-using std::string;
-using std::vector;
+using namespace std;
 
 template <class T>
-class HashTable
-{
+class HashTable{
 public:
-  class KeyValuePair
-  {
+  class KeyValuePair{
     string _key;
     T _value;
-
   public:
-    KeyValuePair(string key, T value) : _key(key), _value(value) {}
-    string &key()
-    {
+    KeyValuePair(string key, T value): _key(key), _value(value){}
+    string& key(){
       return _key;
     }
-    T &value()
-    {
+    T& value(){
       return _value;
     }
   };
 
-  class NodeList
-  {
-    list<KeyValuePair> items;
-
+  class NodeList{
+    std::list<KeyValuePair> items;
   public:
-    void push_front(string key, T &value)
-    {
+    void push_front(string key, T& value) {
       items.push_front(KeyValuePair(key, value));
     }
-    void push_back(string key, T &value)
-    {
+    void push_back(string key, T& value) {
       items.push_back(KeyValuePair(key, value));
     }
-    typename list<KeyValuePair>::iterator begin()
-    {
+    typename list<KeyValuePair>::iterator begin() {
       return items.begin();
     }
-    typename list<KeyValuePair>::iterator end()
-    {
+    typename list<KeyValuePair>::iterator end() {
       return items.end();
     }
-    size_t size()
-    {
+    size_t size() {
       return items.size();
     }
   };
-  class ValueProxy
-  {
+  class ValueProxy {
     string key;
     size_t index;
-    HashTable<T> *hashtable;
+    HashTable<T>* hashtable;
 
-    KeyValuePair &findPair() const
-    {
-      for (KeyValuePair &pair : hashtable->table[index])
-      {
-        if (pair.key() == key)
-        {
+    KeyValuePair& findPair() const {
+      for (KeyValuePair& pair : hashtable->table[index]) {
+        if (pair.key() == key) {
           return pair;
         }
       }
       throw std::invalid_argument("'" + key + "' wasn't found in the HashTable.");
     }
-
   public:
-    ValueProxy(string key, HashTable<T> *hashtable)
-    {
+    ValueProxy(string key, HashTable<T>* hashtable) {
       this->key = key;
       this->index = hashtable->hash(key);
       this->hashtable = hashtable;
     }
 
-    ValueProxy &operator=(T &value)
-    {
-      try
-      {
+    ValueProxy& operator=(T& value) {
+      try {
         KeyValuePair pair = findPair();
         pair.value() = value;
-      }
-      catch (const std::invalid_argument &err)
-      {
+      } catch (const std::invalid_argument& err) {
         hashtable->table[index].push_back(key, value);
       }
       return *this;
     }
 
-    ValueProxy &operator=(T value)
-    {
-      try
-      {
+    ValueProxy& operator=(T value) {
+      try {
         KeyValuePair pair = findPair();
         pair.value() = value;
-      }
-      catch (const std::invalid_argument &err)
-      {
+      } catch (const std::invalid_argument& err) {
         hashtable->table[index].push_back(key, value);
       }
       return *this;
     }
 
-    ValueProxy &operator=(ValueProxy &proxy)
-    {
+    ValueProxy& operator=(ValueProxy& proxy) {
       KeyValuePair pair = proxy.findPair();
       return operator=(&pair.value());
     }
 
-    operator T()
-    {
+    operator T() {
       KeyValuePair pair = findPair();
       return pair.value();
     }
 
-    friend ostream &operator<<(ostream &out, const ValueProxy &proxy)
-    {
+    friend ostream& operator<<(ostream& out, const ValueProxy& proxy) {
       KeyValuePair pair = proxy.findPair();
       out << pair.value();
       return out;
@@ -135,45 +102,34 @@ public:
 private:
   vector<NodeList> table;
   friend class ValueProxy;
-
+  
 public:
-  HashTable(size_t size)
-  {
+  HashTable(size_t size) {
     table = vector<NodeList>(size);
   }
-  void forEach(function<void(KeyValuePair &)> fn)
-  {
-    for (NodeList &list : table)
-    {
-      for (KeyValuePair &pair : list)
-      {
+  void forEach(function<void(KeyValuePair&)> fn) {
+    for (NodeList& list : table) {
+      for (KeyValuePair& pair : list) {
         fn(pair);
       }
     }
   }
-  T &operator[](string key) const
-  {
+  T& operator[](string key) const {
     size_t index = hash(key);
-    for (KeyValuePair &pair : table[index])
-    {
-      if (pair.key() == key)
-      {
+    for (KeyValuePair& pair : table[index]) {
+      if (pair.key() == key) {
         return pair.value();
       }
     }
     throw std::invalid_argument("'" + key + "' wasn't found in the HashTable.");
   }
-  ValueProxy operator[](string key)
-  {
+  ValueProxy operator[](string key) {
     return ValueProxy(key, this);
   }
-
 private:
-  size_t hash(string str)
-  {
+  size_t hash(string str) {
     size_t hash = (size_t)57;
-    for (size_t i = 0; i < str.length(); ++i)
-    {
+    for (size_t i = 0; i < str.length(); ++i) {
       hash = (size_t)19 * hash * (size_t)str.at(i);
     }
     return hash % table.size();
